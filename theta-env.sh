915619a59d894c1cda45147323a0ca91db8fa931
brettin <brettin@anl.gov>
2018-08-08 13:25:16 +0000

    first working candle version

---
+++ theta-env.sh
@@ -0,0 +1,7 @@
+module load miniconda-3.6/conda-4.4.10
+export KMP_BLOCKTIME=0
+export KMP_SETTINGS=0
+export KMP_AFFINITY="granularity=fine,compact,1,0"
+export OMP_NUM_THREADS=128
+export NUM_INTER_THREADS=2
+export NUM_INTRA_THREADS=128
