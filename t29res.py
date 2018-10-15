e50bcb8bae3d1b39667c4b550bc695b3504871d9
brettin <brettin@anl.gov>
2018-10-15 16:29:13 -0500

    removed unnecessary code

--- t29res.py
+++ t29res.py
@@ -94,35 +94,8 @@ def load_data(nb_classes, PL):
         
     return X_train, Y_train, X_test, Y_test
 
-def build_feature_model(input_shape, name='', dense_layers=[1000, 1000],
-                        activation='relu', residual=False,
-                        dropout_rate=0, permanent_dropout=True):
-    x_input = Input(shape=input_shape)
-    h = x_input
-    for i, layer in enumerate(dense_layers):
-        x = h
-        h = Dense(layer, activation=activation)(h)
-        if dropout_rate > 0:
-            if permanent_dropout:
-                h = PermanentDropout(dropout_rate)(h)
-            else:
-                h = Dropout(dropout_rate)(h)
-        if residual:
-            try:
-                h = keras.layers.add([h, x])
-            except ValueError:
-                pass
-    model = Model(x_input, h, name=name)
-    return model
 
-# Input is x
-def g(x):
-    i = x
-    x = Dropout(DR)(x)
-    y = Dense(1000, activation=ACTIVATION)(x)
-    z = ke.layers.add([i,y])
-    return z
-
+# Create residual connections
 # x is input
 # distance is distance to residual connection
 def f(x, gParameters, distance=1):
