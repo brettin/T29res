cdfda682def0cec36b56435c5f269993b4893c6e
brettin <brettin@anl.gov>
2018-10-16 08:19:22 -0500

    working version

--- infer.py
+++ infer.py
@@ -3,9 +3,6 @@ import numpy as np
 import os
 import sys
 import gzip
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
 import keras as ke
 from keras.layers import Input, Dense, Dropout, Activation
 from keras.optimizers import SGD, Adam, RMSprop
@@ -23,6 +20,7 @@ file_path = os.path.dirname(os.path.realpath(__file__))
 sys.path.append('/raid/brettin/Benchmarks/common')
 import candle_keras
 
+# This needs to be fixed
 # candle
 def initialize_parameters():
     t29_common = candle_keras.Benchmark(file_path, 't29_default_model.txt','keras',
@@ -45,7 +43,7 @@ def initialize_parameters():
     return gParameters
 
 
-def load_data(nb_classes, PL, gParameters):
+def load_data(gParameters):
     train_path=gParameters['train_path']
     test_path=gParameters['test_path']
     df_train = (pd.read_csv(train_path,header=None).values).astype('float32')
@@ -57,16 +55,16 @@ def load_data(nb_classes, PL, gParameters):
     df_y_train = df_train[:,0].astype('int')
     df_y_test = df_test[:,0].astype('int')
 
-    Y_train = np_utils.to_categorical(df_y_train,nb_classes)
+    Y_train = np_utils.to_categorical(df_y_train,gParameters['classes'])
     train_classes = np.argmax(Y_train, axis=1)
     np.savetxt("train_classes.csv", train_classes, delimiter=",", fmt="%d")
     
-    Y_test = np_utils.to_categorical(df_y_test,nb_classes)
+    Y_test = np_utils.to_categorical(df_y_test,gParameters['classes'])
     test_classes = np.argmax(Y_test, axis=1)
     np.savetxt("test_classes.csv", test_classes, delimiter=",", fmt="%d")
               
-    df_x_train = df_train[:, 1:PL].astype(np.float32)
-    df_x_test = df_test[:, 1:PL].astype(np.float32)
+    df_x_train = df_train[:, 1:df_train.shape[1]].astype(np.float32)
+    df_x_test = df_test[:, 1:df_train.shape[1]].astype(np.float32)
             
     # not sure the extra variable is needed, and is this a copy or reference
     X_train = df_x_train
@@ -86,7 +84,7 @@ def load_data(nb_classes, PL, gParameters):
 def run(gParameters):
     print ('gParameters: ', gParameters)
 
-    X_train, Y_train, X_test, Y_test = load_data(nb_classes, PL, gParameters)
+    X_train, Y_train, X_test, Y_test = load_data(gParameters)
 
     print('X_train shape:', X_train.shape)
     print('X_test shape:', X_test.shape)
@@ -100,12 +98,6 @@ def run(gParameters):
     json_file.close()
     loaded_model_json = model_from_json(loaded_model_json)
 
-    # load yaml and create model
-    yaml_file = open('t29res.model.yaml', 'r')
-    loaded_model_yaml = yaml_file.read()
-    yaml_file.close()
-    loaded_model_yaml = model_from_yaml(loaded_model_yaml)
-
     # load weights into new model
     loaded_model_json.load_weights("t29res.model.h5")
     print("Loaded json model from disk")
@@ -118,35 +110,23 @@ def run(gParameters):
     print('json Validation accuracy:', score_json[1])
     print("json %s: %.2f%%" % (loaded_model_json.metrics_names[1], score_json[1]*100))
 
-    # load weights into new model
-    loaded_model_yaml.load_weights("t29res.model.h5")
-    print("Loaded yaml model from disk")
-
-    # evaluate loaded model on test data
-    loaded_model_yaml.compile(loss='binary_crossentropy', optimizer=gParameters['optimizer'], metrics=['accuracy'])
-    score_yaml = loaded_model_yaml.evaluate(X_test, Y_test, verbose=0)
-
-    print('yaml Validation loss:', score_yaml[0])
-    print('yaml Validation accuracy:', score_yaml[1])
-    print("yaml %s: %.2f%%" % (loaded_model_yaml.metrics_names[1], score_yaml[1]*100))
-
     # predict using loaded yaml model on test and training data
-    predict_yaml_train = loaded_model_yaml.predict(X_train)
-    predict_yaml_test = loaded_model_yaml.predict(X_test)
+    predict_json_train = loaded_model_json.predict(X_train)
+    predict_json_test = loaded_model_json.predict(X_test)
 
-    print('Yaml_train_shape:', predict_yaml_train.shape)
-    print('Yaml_test_shape:', predict_yaml_test.shape)
+    print('predict_train_train:', predict_json_train.shape)
+    print('predict_test_shape:', predict_json_test.shape)
 
-    predict_yaml_train_classes = np.argmax(predict_yaml_train, axis=1)
-    predict_yaml_test_classes = np.argmax(predict_yaml_test, axis=1)
+    predict_json_train_classes = np.argmax(predict_json_train, axis=1)
+    predict_json_test_classes = np.argmax(predict_json_test, axis=1)
 
-    np.savetxt("predict_yaml_train.csv", predict_yaml_train, delimiter=",", fmt="%.3f")
-    np.savetxt("predict_yaml_test.csv", predict_yaml_test, delimiter=",", fmt="%.3f")
+    np.savetxt("predict_json_train.csv", predict_json_train, delimiter=",", fmt="%.3f")
+    np.savetxt("predict_json_test.csv", predict_json_test, delimiter=",", fmt="%.3f")
 
-    np.savetxt("predict_yaml_train_classes.csv", predict_yaml_train_classes, delimiter=",",fmt="%d")
-    np.savetxt("predict_yaml_test_classes.csv", predict_yaml_test_classes, delimiter=",",fmt="%d")
+    np.savetxt("predict_json_train_classes.csv", predict_json_train_classes, delimiter=",",fmt="%d")
+    np.savetxt("predict_json_test_classes.csv", predict_json_test_classes, delimiter=",",fmt="%d")
 
-    return history
+    return
 
 # This is also added for candle compliance so that the program can
 # still be executed independently from the command line.
